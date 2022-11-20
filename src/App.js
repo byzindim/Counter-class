0310e51a552ce06b7f647ad1ff5fe3994fd5697f
redlinesoft <info@redlinesoft.ru>
2022-11-20 16:14:27 +0700

    counter-class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: this.props.counter
+    }
+  }
+  incChange = () => {
+    this.setState(state  => ({
+      counter: this.state.counter - 1
+    }))
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  decChange = () => {
+    this.setState(state => ({
+      counter: this.state.counter + 1
+    }))
+  }
+
+  randomIntFromInterval = () => { 
+    this.setState(state => ({
+      counter: +Math.floor(Math.random() * (50 - (-50) + 1) + (-50))
+    }))
+  }
+  
+  resetChange = () => {
+    this.setState(state => ({
+      counter: this.props.counter
+    }))
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="counter">{this.state.counter}</div>
+        <div className="controls">
+          <button onClick={this.incChange}>INC</button>
+          <button onClick={this.decChange}>DEC</button>
+          <button onClick={this.randomIntFromInterval}>RND</button>
+          <button onClick={this.resetChange}>RESET</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;

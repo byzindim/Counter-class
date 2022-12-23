0b58f5cbd8c64425627f1d028c0906d51430154d
redlinesoft <info@redlinesoft.ru>
2022-12-23 16:17:37 +0700

    add-my-hook

--- src/App.js
+++ src/App.js
@@ -6,21 +6,26 @@ import CourseMoney from './services/CourseMoney'
 
 const App = () => {
   const valuteService = new CourseMoney();
-  const [inputValue, setInputValue] = useState(0);
-  const [textResult, setTextResult] = useState(0);
-  const [valuteValue, setValuteValue] = useState();
-  const [valuteCode, setValuteCode] = useState();
+
+  function useAddHook(initialValue) {
+    const [value, setValue] = useState(initialValue)
+    const onChange = (e) => {
+      setValue(e.target.value)
+    }
+    return {value, onChange, setValue}
+  }
+
+  const input = useAddHook(''),
+    result = useAddHook(''),
+    convert = useAddHook(''),
+    valute = useAddHook('');
 
   useEffect(() => {
     onValute()
   },[])
 
-  const onInputValue = (e) => {
-    setInputValue(Number(e.target.value)) 
-  }
-
   const convertValute = () => {
-    setTextResult((inputValue/valuteValue).toFixed(4)) 
+    result.setValue((input.value/convert.value).toFixed(4)) 
   }
   const onValute = () => {
     valuteService
@@ -28,26 +33,25 @@ const App = () => {
       .then(onCourse)
   }
   const onCourse = ({valuteValue, valuteCode}) => {
-    setValuteValue(valuteValue)
-    setValuteCode(valuteCode)
+    convert.setValue(valuteValue)
+    valute.setValue(valuteCode)
   }
 
   const onReset = () => {
-    setInputValue(0);
-    setTextResult(0);
+    input.setValue(0);
+    result.setValue(0);
   }
   return (
     <div className="app">
-      <input type="text" value={inputValue} onChange={onInputValue}/>
-      <input type="text" value={textResult} />
+      <input type="text" value={input.value} onChange={input.onChange}/>
+      <input type="text" value={result.value} onChange={result.onChange}/>
       <div className="convert">{null}</div>
       <div className="controls">
-        <button onClick={convertValute}>{valuteCode}</button>
+        <button onClick={convertValute}>{valute.value}</button>
         <button onClick={onReset}>RESET</button>
       </div>
     </div>
-  )
-   
+  ) 
 }
 
 export default App;
